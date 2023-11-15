654428e41b093ac2000dc4ebce21b86a79ddded4
Joseph DeWeese <jdeweese@weldtascics.systems>
2023-11-15 13:35:40 -0600

    Initial Commit

---
+++ Fiscavis1.5UITests/Fiscavis1_5UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Fiscavis1_5UITestsLaunchTests.swift
+//  Fiscavis1.5UITests
+//
+//  Created by J. DeWeese on 11/15/23.
+//
+
+import XCTest
+
+final class Fiscavis1_5UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
